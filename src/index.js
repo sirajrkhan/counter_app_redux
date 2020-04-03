d71ce63a01be368e726de06bad4a29d7c9912e79
Siraj Khan <sirajrkhan@live.in>
2020-04-03 12:48:47 +0530

    middleware and redux devtool config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
@@ -8,7 +8,30 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching... ', action);
+            const result  = next(action);
+            console.log('[Middleware] Next State... ', store.getState());
+            return result;
+        }
+    }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    reducer,
+    composeEnhancers(applyMiddleware(logger))
+);
+
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
